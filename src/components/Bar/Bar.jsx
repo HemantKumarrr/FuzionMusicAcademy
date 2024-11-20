b1975d619bec16b4091ec44d8e4d7b21ac28cea5
Hemant <hemantkumar29052001@gmail.com>
2024-11-21 01:14:45 +0530

    bar updated

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -6,7 +6,7 @@ const Bar = () => {
     <div className="card text-white">
       <Link
         to="https://wa.me/message/LZIPHDYS54GHC1"
-        className="bg-green-700 w-[3.4rem] p-2 flex items-start justify-center rounded-full"
+        className="bg-green-600 w-[3.4rem] p-2 flex items-start justify-center rounded-full"
       >
         <FaWhatsapp className="text-4xl" />
       </Link>
